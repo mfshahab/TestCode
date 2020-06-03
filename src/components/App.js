5c13a5a46f12e317339a1e3648d216103d04e115
mfshahab <66128030+mfshahab@users.noreply.github.com>
2020-06-04 00:57:34 +0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<h4 className="center-align animated fadeInUp">Hey !</h4>
+			</div>
+		);
+	}
+}
